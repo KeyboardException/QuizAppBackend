6ba0a420740a0276b7fe0a2994c5d6d378465449
Belikhun <belivipro9x99@gmail.com>
2023-11-01 07:12:30 +0700

    🧊 add endpoint for fetching user data

--- classes/Controller/User.php
+++ classes/Controller/User.php
@@ -5,11 +5,20 @@ namespace Controller;
 use Blink\Request;
 use Blink\Response\APIResponse;
 use Blink\Session;
+use Model\QuestionAttempt;
 
 class User {
 	public static function get(Request $request, int $id) {
 		$user = \User::getByID($id);
-		return new APIResponse(0, "Success!", 200, $user);
+		$data = $user -> jsonSerialize();
+		$data["correct"] = QuestionAttempt::query()
+			-> leftJoin("attempts", "question_attempts.attempt_id", "attempts.id")
+			-> where("attempts.user_id", $user -> id)
+			-> where("question_attempts.status", QuestionAttempt::ANSWERED)
+			-> where("question_attempts.correct", true)
+			-> count();
+
+		return new APIResponse(0, "Success!", 200, $data);
 	}
 
 	public static function ranking() {
